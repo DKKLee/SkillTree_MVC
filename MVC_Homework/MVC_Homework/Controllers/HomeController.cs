3e67913c814d26ab1cb195d3555cdcb980237035
DKKLee <haynike1029@gmail.com>
2019-07-11 00:47:02 +0800

    second

--- MVC_Homework/MVC_Homework/Controllers/HomeController.cs
+++ MVC_Homework/MVC_Homework/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC_Homework.Models;
 
 namespace MVC_Homework.Controllers
 {
@@ -10,8 +11,27 @@ namespace MVC_Homework.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            List<Class1> ViewModl = new List<Class1>();
+            Random rMoney = new Random() ;
+            for(int i = 1; i <= 100; i++)
+            {
+                ViewModl.Add(new Class1 { Id = i, Type = "支出", Date = DateTime.Now.ToString("yyyy-MM-dd"), Money = rMoney.Next(1,2000) });
+            }
+
+            //var result = new Class1
+            //{
+            //    Id = 1,
+            //    Type = "支出",
+            //    Date = string.Format("{0:yyyy-MM-dd}",DateTime.Now),
+            //    Money = 1600
+            //};
+            return View(ViewModl);
         }
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
 
         public ActionResult About()
         {
---
+++ MVC_Homework/MVC_Homework/Models/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Homework.Models
+{
+    public class Class1
+    {
+        public int  Id { get; set; }
+        public string Type { get; set; }
+        public string Date{ get; set; }
+        public int Money{ get; set; }
+
+    }
+}
